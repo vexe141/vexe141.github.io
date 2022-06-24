5eb65ed8a4e3249848d7a8de66783166291eb8ff
vexe <sadbhavadhikari@gmail.com>
2022-06-24 16:12:46 +0000

    fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vexe141/",
+  base: "/about/",
   plugins: [svelte()]
 });

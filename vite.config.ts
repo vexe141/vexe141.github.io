9e62e8cbd0369a86ff5c2689aad603e6ef9e5c59
vexe <sadbhavadhikari@gmail.com>
2022-07-18 11:07:55 +0545

    [updated] build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+      rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html")
+	  }
+    }
+  },
   plugins: [svelte()]
 });
